62e5f8016acf7b5a6e01e2ddf7390a66eb7cbaa4
voidvisor <43892995+voidvisor@users.noreply.github.com>
2023-04-02 16:51:28 +0300

    Nominacijas dropdown
    Pārejam no kartiņu uz dropdown sistēmu nominācijām.

--- src/components/balss.tsx
+++ src/components/balss.tsx
@@ -2,29 +2,24 @@ import '../style/Balss.css'
 import {useSessionStorage} from './sessionStorage.js'
 import Login from './login'
 import Nominacija from './nominacija'
-import { useState } from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 
 export default function Balss() {
   const [validSession, setSession] = useSessionStorage('validSession', '')
-  const [curNom, setNom] = useState(0)
-  const nominacijas = [{title: 'Skurstenis', description: 'Lorem Ipsum', img: 'https://picsum.photos/500'},{title: 'Skolotāju mīlulis', description: 'Lorem Ipsum', img: 'https://picsum.photos/500'},{title: 'Melomāns', description: 'Lorem Ipsum', img: 'https://picsum.photos/500'}]
-  const nomarr = []
-
-  for (let i = 0; i < nominacijas.length; i++) {
-    nomarr.push(<Nominacija title={nominacijas[i].title} desc={nominacijas[i].description} img={nominacijas[i].img} />)
-  }
+  const nominacijas = [{title: 'Skurstenis', description: 'Lorem Ipsum'},{title: 'Skolotāju mīlulis', description: 'Lorem Ipsum'},{title: 'Melomāns', description: 'Lorem Ipsum'}]
+  const nomarr = nominacijas.map(nominacija => <Nominacija title={nominacija.title} desc={nominacija.description} />)
 
   return (
     <div className="Balss" id='balssDiv'>
       {!validSession && <Login setSession={setSession} />}
-      <div className='mx-auto' id='nomDiv'>
-          {nomarr[curNom]}
-          <div id='buttonDiv'>
-            {curNom > 0 && <Button variant='primary' id='prev' onClick={() => {setNom(curNom-1)}}>Iepriekšējais</Button>}
-            {curNom < nominacijas.length - 1 && <Button variant='primary' id='next' onClick={() => {setNom(curNom+1)}}>Nākamais</Button>}
-        </div>
-      </div>
+      <Form>
+        {nomarr}
+        <center>
+          <Button variant="primary" type="submit">
+            Balsot
+          </Button>
+        </center>
+      </Form>
     </div>
   )
 }
--- src/components/nominacija.tsx
+++ src/components/nominacija.tsx
@@ -1,16 +1,37 @@
-import { Card } from "react-bootstrap";
+import { useState } from "react";
+import { Dropdown, DropdownButton, Form, InputGroup } from "react-bootstrap";
 import '../style/nominacija.css'
 
-export default function Nominacija({title, desc, img}) {
+export default function Nominacija({title, desc}) {
+const klases = ["12.1", "12.2", "12.3", "12.4", "12.5", "12.6"]
+const skoleni = [{klase: "12.1", skoleni: ["fizikis", "matematikis"]}, {klase: "12.2", skoleni: ["biodio", "big pharma"]}, {klase: "12.3", skoleni: ["ultra biodio", "small pharma"]}]
+const [klase, setKlase] = useState("Klase")
+
+const mapSkoleni = () => {
+    for (let i = 0; i < skoleni.length; i++) {
+        if (skoleni[i].klase === klase) {
+            return skoleni[i].skoleni.map(skolens => <option value={`${klase}-${skolens}`}>{skolens}</option>)
+        }
+    }
+}
+
     return (
-        <div className="nominacija mb-3">
-            <Card id='nomCard'>
-                <Card.Img id='nomImg' variant='top' src={img} />
-                <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>{desc}</Card.Text>
-                </Card.Body>
-            </Card>
-        </div>
+        <Form.Group className="nominacija mb-3">
+            <Form.Label className="h5">{title}</Form.Label>
+            <br />
+            <Form.Text className="text-muted">{desc}</Form.Text>
+            <InputGroup>
+                <DropdownButton
+                    variant="outline-dark"
+                    title={klase}
+                >
+                    {klases.map(klase => <Dropdown.Item value={klase} onClick={() => {setKlase(klase)}}>{klase}</Dropdown.Item>)}
+                </DropdownButton>
+                <Form.Select>
+                    <option>Izvēlies skolēnu</option>
+                    {mapSkoleni()}
+                </Form.Select>
+            </InputGroup>
+        </Form.Group>
     )
 }
