82aefee670dc8e4271e496e367b2d8e8d1ef0f57
voidvisor <43892995+voidvisor@users.noreply.github.com>
2023-04-03 21:51:01 +0300

    Pievienojam skolotāju nominācijas

--- src/components/balss.tsx
+++ src/components/balss.tsx
@@ -8,14 +8,25 @@ import { useContext } from 'react'
 
 export default function Balss() {
   const [validSession, setSession] = useSessionStorage('validSession', false)
-  const nomarr = nominacijas.map(nominacija => <Nominacija key={nominacija.title} title={nominacija.title} desc={nominacija.description} />)
+  const [code, setCode] = useSessionStorage('voteCode', '')
   const nominacijas = useContext(nominContext)
+  const skolenNomi = nominacijas.skoleni
+  const skolotNomi = nominacijas.skolotaji
+  const skolenNomarr = skolenNomi.map(nominacija => <Nominacija key={nominacija.title} title={nominacija.title} desc={nominacija.desc} skolens={true} />)
+  const skolotNomarr = skolotNomi.map(nominacija => <Nominacija key={nominacija.title} title={nominacija.title} desc={nominacija.desc} skolens={false} />)
 
   return (
-    <div className="Balss" id='balssDiv'>
-      {!validSession && <Login setSession={setSession} />}
-      <Form>
-        {nomarr}
+    <div id='balssDiv'>
+      {!validSession && <Login setSession={setSession} code={code} setCode={setCode} />}
+      <Form className='m-3'>
+        <center>
+          <h3>Skolēnu nominācijas</h3>
+        </center>
+        {skolenNomarr}
+        <center>
+          <h3>Skolotāju nominācijas</h3>
+        </center>
+        {skolotNomarr}
         <center>
           <Button variant="primary" type="submit">
             Balsot
--- src/components/nominacija.tsx
+++ src/components/nominacija.tsx
@@ -2,36 +2,43 @@ import { useState } from "react";
 import { Dropdown, DropdownButton, Form, InputGroup } from "react-bootstrap";
 import '../style/nominacija.css'
 
-export default function Nominacija({title, desc}) {
-const klases = ["12.1", "12.2", "12.3", "12.4", "12.5", "12.6"]
-const skoleni = [{klase: "12.1", skoleni: ["fizikis", "matematikis"]}, {klase: "12.2", skoleni: ["biodio", "big pharma"]}, {klase: "12.3", skoleni: ["ultra biodio", "small pharma"]}]
-const [klase, setKlase] = useState("Klase")
+export default function Nominacija({title, desc, skolens}) {
+    const klases = ["12.1", "12.2", "12.3", "12.4", "12.5", "12.6"]
+    const klasarr = klases.map(klase => <Dropdown.Item key={klase} value={klase} onClick={() => {setKlase(klase)}}>{klase}</Dropdown.Item>)
+    const skoleni = [{klase: "12.1", skoleni: ["fizikis", "matematikis"]}, {klase: "12.2", skoleni: ["biodio", "big pharma"]}, {klase: "12.3", skoleni: ["ultra biodio", "small pharma"]}]
+    const skolotaji = ['Lankovska', 'Mihno']
+    const skolotarr = skolotaji.map(skolotajs => <option key={skolotajs} value={skolotajs}>{skolotajs}</option>)
+    const [klase, setKlase] = useState("Klase")
 
-const mapSkoleni = () => {
-    for (let i = 0; i < skoleni.length; i++) {
-        if (skoleni[i].klase === klase) {
-            return skoleni[i].skoleni.map(skolens => <option key={skolens} value={`${klase}-${skolens}`}>{skolens}</option>)
+    const mapSkoleni = () => {
+        for (let i = 0; i < skoleni.length; i++) {
+            if (skoleni[i].klase === klase) {
+                return skoleni[i].skoleni.map(skolens => <option key={skolens} value={`${klase}-${skolens}`}>{skolens}</option>)
+            }
         }
     }
-}
 
     return (
         <Form.Group className="nominacija mb-3">
             <Form.Label className="h5">{title}</Form.Label>
             <br />
             <Form.Text className="text-muted">{desc}</Form.Text>
-            <InputGroup>
-                <DropdownButton
-                    variant="outline-dark"
-                    title={klase}
-                >
-                    {klases.map(klase => <Dropdown.Item key={klase} value={klase} onClick={() => {setKlase(klase)}}>{klase}</Dropdown.Item>)}
-                </DropdownButton>
+            {skolens === true ? 
+                <InputGroup>
+                    <DropdownButton variant="outline-dark" title={klase}>
+                        {klasarr}
+                    </DropdownButton>
+                    <Form.Select>
+                        <option>Izvēlies skolēnu</option>
+                        {mapSkoleni()}
+                    </Form.Select>
+                </InputGroup>
+            :
                 <Form.Select>
-                    <option>Izvēlies skolēnu</option>
-                    {mapSkoleni()}
+                    <option>Izvēlies skolotāju</option>
+                    {skolotarr}
                 </Form.Select>
-            </InputGroup>
+            }
         </Form.Group>
     )
 }
