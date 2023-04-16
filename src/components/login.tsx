10d78411e2a58c1a3f73136b03e3f19849ee966c
voidvisor <43892995+voidvisor@users.noreply.github.com>
2023-04-16 13:06:29 +0300

    Pieliekam admin login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,15 +1,25 @@
 import { Alert, Button, Card, FloatingLabel, Form } from 'react-bootstrap';
 import '../style/login.css'
 import { useState } from 'react';
-import { app } from '../main';
+import { app as regApp } from '../routes/root';
+import { app as adminApp } from '../routes/admin';
+import * as Realm from "realm-web";
 
-export default function Login({setSession, code, setCode}) {
+interface loginProps {
+    setSession: (session: boolean) => void;
+    code?: string;
+    setCode?: (code: string) => void;
+    type: 'reg' | 'admin';
+}
+
+export default function Login({setSession, code, setCode, type}: loginProps) {
     const [alert, showAlert] = useState(false)
     const [alertMsg, setAlertMsg] = useState('')
+    const [apiKey, setApiKey] = useState('')
 
-    async function onSubmit(e) {
+    async function onRegSubmit(e) {
         e.preventDefault();
-        app.currentUser.functions.authFunc(code).then(
+        regApp.currentUser.functions.authFunc(code).then(
             (result) => {
                 if (result === 'invalid') {
                     showAlert(true)
@@ -34,16 +44,55 @@ export default function Login({setSession, code, setCode}) {
         )
     }
 
-    return (
-        <div id='backDiv'>
+    async function onAdminSubmit(e) {
+        e.preventDefault();
+        console.log(apiKey)
+        const credentials = Realm.Credentials.apiKey(apiKey);
+        console.log(credentials)
+        const user = adminApp.logIn(credentials).then(
+            (result) => {
+                setSession(true)
+            },
+            (error) => {
+                showAlert(true)
+                setAlertMsg(`Sistēmas kļūda: ${error}`)
+            }
+        )
+    }
+
+    if (type === 'reg') {
+        return (
+            <div id='backDiv'>
+                <div id='loginDiv'>
+                    <Card className='m-auto' id='loginCard'>
+                        <Card.Header>Autorizācija</Card.Header>
+                        <Card.Body>
+                            <Form onSubmit={onRegSubmit}>
+                                <Form.Group className='mb-3'>
+                                    <FloatingLabel label='Autentifikācijas kods'>
+                                        <Form.Control type='password' placeholder='Autentifikācijas kods' value={code} onChange={e => setCode(e.target.value)} />
+                                    </FloatingLabel>
+                                </Form.Group>
+                                <center>
+                                    <Button variant='primary' type='submit'>Autorizēties</Button>
+                                </center>
+                            </Form>
+                            {alert && <Alert className='mt-3' variant='danger'>{alertMsg}</Alert>}
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+        )
+    } else if (type === 'admin') {
+        return (
             <div id='loginDiv'>
                 <Card className='m-auto' id='loginCard'>
                     <Card.Header>Autorizācija</Card.Header>
                     <Card.Body>
-                        <Form onSubmit={onSubmit}>
+                        <Form onSubmit={onAdminSubmit}>
                             <Form.Group className='mb-3'>
                                 <FloatingLabel label='Autentifikācijas kods'>
-                                    <Form.Control type='password' placeholder='Autentifikācijas kods' value={code} onChange={e => setCode(e.target.value)} />
+                                    <Form.Control type='password' placeholder='Autentifikācijas kods' value={apiKey} onChange={e => {setApiKey(e.target.value);console.log(e)}} />
                                 </FloatingLabel>
                             </Form.Group>
                             <center>
@@ -54,6 +103,6 @@ export default function Login({setSession, code, setCode}) {
                     </Card.Body>
                 </Card>
             </div>
-        </div>
-    )
+        )
+    }
 }
