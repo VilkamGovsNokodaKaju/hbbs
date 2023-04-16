12f67217e375e5646d3e0423fdf7c87a97d47ec2
voidvisor <43892995+voidvisor@users.noreply.github.com>
2023-04-16 13:07:51 +0300

    Atbalstam router

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Balss from './components/balss'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { balssContext, klasContext, nominContext, skolotContext, stampContext } from './components/contextProvider'
-import * as Realm from "realm-web";
-
-const realmApp = "hbbs-ntiaq"
-const mongoCluster = "mongodb-atlas"
-
-const app = new Realm.App({ id: realmApp });
-const credentials = Realm.Credentials.anonymous();
-try {
-  const user = await app.logIn(credentials);
-} catch(err) {
-  console.error("Failed to log in", err);
-}
-
-const mongo = app.currentUser.mongoClient(mongoCluster);
-const sistema = mongo.db('data').collection('sistema')
-const sistemasDati = await sistema.find()
-const stamps = sistemasDati.find(doc => doc._id === 'stamps')
-const nominacijas = sistemasDati.find(doc => doc._id === 'nominacijas')
-const klases = sistemasDati.find(doc => doc._id === 'klases')
-const skolotaji = sistemasDati.find(doc => doc._id === 'skolotaji')
-const balsis = mongo.db('data').collection('balsis')
-const balssDati = await balsis.find()
+import Router from './components/router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <stampContext.Provider value={stamps}>
-      <nominContext.Provider value={nominacijas}>
-        <klasContext.Provider value={klases.list}>
-          <skolotContext.Provider value={skolotaji.list}>
-            <balssContext.Provider value={balssDati}>
-              <Balss />
-            </balssContext.Provider>
-          </skolotContext.Provider>
-        </klasContext.Provider>
-      </nominContext.Provider>
-    </stampContext.Provider>
+    <Router url={window.location.pathname} />
   </React.StrictMode>
 )
-
-export { app }
