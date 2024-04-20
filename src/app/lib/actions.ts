806223f0f6b0d9e58c20d85e9fa03eb98dd79a41
voidvisor <43892995+voidvisor@users.noreply.github.com>
2024-04-20 20:55:59 +0300

    Datubāzes idleTimeout 60

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -9,7 +9,8 @@ const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  idleTimeout: 60,
 });
 
 process.on("SIGINT", async function() {
