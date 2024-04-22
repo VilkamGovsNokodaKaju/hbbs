79888832d8956af140cc3beed241e0d8f25156ea
voidvisor <43892995+voidvisor@users.noreply.github.com>
2024-04-22 11:07:52 +0300

    Izmantot pareizo url priekš redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,11 @@ export function middleware(request: NextRequest) {
     const code = request.nextUrl.searchParams.get("code")
 
     if (currentUser && request.nextUrl.pathname.startsWith("/login")) {
-        return Response.redirect(new URL("/", request.url))
+        return Response.redirect(new URL("/", request.nextUrl))
     }
 
     if (code && request.nextUrl.pathname.startsWith("/login")) {
-        const response = NextResponse.redirect(new URL("/", request.url))
+        const response = NextResponse.redirect(new URL("/", request.nextUrl))
         response.cookies.set("user", code, { secure: true, httpOnly: true, sameSite: 'strict', maxAge: 60 * 60 * 24 * 1 })
         return response
     }
